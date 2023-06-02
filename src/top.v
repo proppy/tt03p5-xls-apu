f92f8f2ecabbbfdc397703b28cfc05c60ac8d1e9
Johan Euphrosine <proppy@google.com>
2023-06-03 00:55:36 +0900

    top: add 2 more oscillators

--- src/top.v
+++ src/top.v
@@ -25,36 +25,44 @@ module tt_um_apu_pulse (
 	input  wire       rst_n
 );
 
-   wire [10:0] 		  period = {uio_in[2:0], ui_in[7:0]};
-   wire 		  period_valid = uio_in[5];
-   wire 		  period_ready;
+   wire [10:0]		  period = {uio_in[2:0], ui_in[7:0]};
+   wire			  period_valid = uio_in[5];
+   wire			  period_ready;
    assign uo_out[2] = period_ready;
-   
-   wire [1:0] 		  duty = uio_in[4:3];
-   wire 		  duty_valid = uio_in[6];
-   wire 		  duty_ready;
-   assign uo_out[3] = duty_ready;
-   
-   wire 		  signal0;
-   assign uo_out[0] = signal0;
-   wire 		  signal0_valid;
-   assign uo_out[1] = signal0_valid;
-   wire 		  signal0_ready = uio_in[7];
-   
-   wire 		  signal1;
-   assign uo_out[4] = signal1;
-   wire 		  signal1_valid;
-   assign uo_out[5] = signal1_valid;
-   wire 		  signal1_ready = uio_in[7];
 
-   wire 		  signal2;
-   assign uo_out[6] = signal2;
-   wire 		  signal2_valid;
-   assign uo_out[7] = signal2_valid;
-   wire 		  signal2_ready = uio_in[7];
+   wire [1:0]		  duty = uio_in[4:3];
+   wire			  duty_valid = uio_in[6];
+   wire			  duty_ready;
+   assign uo_out[3] = duty_ready;
+
+   wire			  signal0;
+   assign uo_out[0] = signal0;
+   wire			  signal0_valid;
+   assign uo_out[1] = signal0_valid;
+   wire			  signal0_ready = uio_in[7];
+
+   wire			  signal1;
+   assign uo_out[4] = signal1;
+   wire			  signal1_valid;
+   wire			  signal1_ready = uio_in[7];
+
+   wire			  signal2;
+   assign uo_out[5] = signal2;
+   wire			  signal2_valid;
+   wire			  signal2_ready = uio_in[7];
+
+   wire			  signal3;
+   assign uo_out[6] = signal3;
+   wire			  signal3_valid;
+   wire			  signal3_ready = uio_in[7];
+
+   wire			  signal4;
+   assign uo_out[7] = signal4;
+   wire			  signal4_valid;
+   wire			  signal4_ready = uio_in[7];
 
    assign uio_oe = 8'h00;
-   
+
    apu_pulse apu_pulse0(.clk(clk),
 			.reset(rst_n),
 			.apu__period_r(period),
@@ -62,7 +70,7 @@ module tt_um_apu_pulse (
 			.apu__period_r_rdy(period_ready),
 			.apu__duty_r(duty),
 			.apu__duty_r_vld(duty_valid),
-			.apu__duty_r_rdy(duty_ready),			
+			.apu__duty_r_rdy(duty_ready),
 			.apu__output_s(signal0),
 			.apu__output_s_vld(signal0_valid),
 			.apu__output_s_rdy(signal0_ready));
@@ -74,7 +82,7 @@ module tt_um_apu_pulse (
 			.apu__period_r_rdy(period_ready),
 			.apu__duty_r(duty),
 			.apu__duty_r_vld(duty_valid),
-			.apu__duty_r_rdy(duty_ready),			
+			.apu__duty_r_rdy(duty_ready),
 			.apu__output_s(signal1),
 			.apu__output_s_vld(signal1_valid),
 			.apu__output_s_rdy(signal1_ready));
@@ -86,9 +94,33 @@ module tt_um_apu_pulse (
 			.apu__period_r_rdy(period_ready),
 			.apu__duty_r(duty),
 			.apu__duty_r_vld(duty_valid),
-			.apu__duty_r_rdy(duty_ready),			
+			.apu__duty_r_rdy(duty_ready),
 			.apu__output_s(signal2),
 			.apu__output_s_vld(signal2_valid),
 			.apu__output_s_rdy(signal2_ready));
+
+   apu_pulse apu_pulse3(.clk(clk),
+			.reset(rst_n),
+			.apu__period_r(period << 2),
+			.apu__period_r_vld(period_valid),
+			.apu__period_r_rdy(period_ready),
+			.apu__duty_r(duty),
+			.apu__duty_r_vld(duty_valid),
+			.apu__duty_r_rdy(duty_ready),
+			.apu__output_s(signal3),
+			.apu__output_s_vld(signal3_valid),
+			.apu__output_s_rdy(signal3_ready));
+
+   apu_pulse apu_pulse4(.clk(clk),
+			.reset(rst_n),
+			.apu__period_r(period >> 2),
+			.apu__period_r_vld(period_valid),
+			.apu__period_r_rdy(period_ready),
+			.apu__duty_r(duty),
+			.apu__duty_r_vld(duty_valid),
+			.apu__duty_r_rdy(duty_ready),
+			.apu__output_s(signal4),
+			.apu__output_s_vld(signal4_valid),
+			.apu__output_s_rdy(signal4_ready));
 
 endmodule // tt_um_apu_pulse
