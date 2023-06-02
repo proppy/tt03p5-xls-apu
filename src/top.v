5423b83c4cd7a16d8278e144c4215a8699556111
Johan Euphrosine <proppy@google.com>
2023-06-02 22:28:58 +0900

    top: rename module

--- src/top.v
+++ src/top.v
@@ -14,7 +14,7 @@
 
 `default_nettype none
 
-module tt_apu_pulse (
+module tt_um_apu_pulse (
 	input  wire [7:0] ui_in,	// Dedicated inputs
 	output wire [7:0] uo_out,	// Dedicated outputs
 	input  wire [7:0] uio_in,	// IOs: Input path
